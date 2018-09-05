df80857925dbc4d0121a52ed232a51e971738681
Swen Wenzel <swen.wenzel@real-digital.de>
2018-09-05 14:09:07 +0200

    DEV-11600: make test_examples run in container

--- test/test_examples.py
+++ test/test_examples.py
@@ -2,7 +2,6 @@ import json
 import pathlib
 import shutil
 import subprocess
-import sys
 
 import pytest
 
@@ -13,31 +12,37 @@ EXAMPLES_DIR = ROOT_DIR / 'examples'
 
 @pytest.fixture
 def tmp_with_pyconnect(tmpdir):
-    python = f'python{sys.version_info.major}.{sys.version_info.minor}'
-    subprocess.run(['virtualenv', f'--python={python}', '.test_venv'], cwd=tmpdir, check=True)
-    subprocess.run(['.test_venv/bin/pip', 'install', ROOT_DIR], cwd=tmpdir, check=True)
-    return pathlib.Path(tmpdir)
+    tmpdir = pathlib.Path(tmpdir).absolute()
+    venv_name = '.test_venv'
+    venv_bin = tmpdir / venv_name / 'bin'
+
+    subprocess.run(['python', '-m', 'venv', venv_name], cwd=tmpdir, check=True)
+    subprocess.run([venv_bin / 'pip', 'install', ROOT_DIR], cwd=tmpdir, check=True)
+    return tmpdir, venv_bin
 
 
 @pytest.mark.e2e
 def test_file_sink_example(cluster_hosts, topic, produced_messages, tmp_with_pyconnect):
+    tmpdir, venv_bin = tmp_with_pyconnect
+    sinkfile = tmpdir / 'sink_dir' / 'sinkfile'
+
     env_vars = {
         'PYCONNECT_BOOTSTRAP_SERVERS': cluster_hosts['broker'],
         'PYCONNECT_SCHEMA_REGISTRY': cluster_hosts['schema-registry'],
         'PYCONNECT_TOPICS': topic[0],
         'PYCONNECT_GROUP_ID': 'testgroup',
-        'PYCONNECT_SINK_DIRECTORY': tmp_with_pyconnect / 'sink_dir',
-        'PYCONNECT_SINK_FILENAME': 'sinkfile'
+        'PYCONNECT_SINK_DIRECTORY': sinkfile.parent,
+        'PYCONNECT_SINK_FILENAME': sinkfile.name
     }
 
-    shutil.copy(EXAMPLES_DIR / 'file_sink' / 'file_sink.py', tmp_with_pyconnect)
-    subprocess.run(['.test_venv/bin/python', 'file_sink.py', '--config', 'env'], env=env_vars,
-                   cwd=tmp_with_pyconnect, check=True)
+    shutil.copy(EXAMPLES_DIR / 'file_sink' / 'file_sink.py', tmpdir)
 
-    with open(tmp_with_pyconnect / 'sink_dir' / 'sinkfile', 'r') as infile:
-        lines = infile.readlines()
+    subprocess.run([venv_bin / 'python', 'file_sink.py', '--config', 'env'], env=env_vars,
+                   cwd=tmpdir, check=True)
 
-    saved_records = [json.loads(line) for line in lines]
+    filedata = sinkfile.read_text()
+
+    saved_records = [json.loads(line) for line in filedata.splitlines()]
 
     for key, value in produced_messages:
         assert {'key': key, 'value': value} in saved_records
@@ -45,24 +50,29 @@ def test_file_sink_example(cluster_hosts, topic, produced_messages, tmp_with_pyc
 
 @pytest.mark.e2e
 def test_file_source_example(records, cluster_hosts, topic, consume_all, tmp_with_pyconnect):
-    source_dir = tmp_with_pyconnect / 'source_dir'
+    tmpdir, venv_bin = tmp_with_pyconnect
+    source_file = tmpdir / 'source_dir' / 'sourcefile'
+
     env_vars = {
         'PYCONNECT_BOOTSTRAP_SERVERS': cluster_hosts['broker'],
         'PYCONNECT_SCHEMA_REGISTRY': cluster_hosts['schema-registry'],
         'PYCONNECT_TOPIC': topic[0],
         'PYCONNECT_OFFSET_TOPIC': topic[0] + '_offset_topic',
-        'PYCONNECT_SOURCE_DIRECTORY': source_dir,
-        'PYCONNECT_SOURCE_FILENAME': 'sourcefile'
+        'PYCONNECT_SOURCE_DIRECTORY': source_file.parent,
+        'PYCONNECT_SOURCE_FILENAME': source_file.name
     }
 
-    source_dir.mkdir(parents=True, exist_ok=True)
-    with open(source_dir / 'sourcefile', 'w') as outfile:
-        for key, value in records:
-            outfile.write(json.dumps({'key': key, 'value': value}) + '\n')
+    source_file.parent.mkdir(parents=True, exist_ok=True)
+    source_data = '\n'.join(
+        json.dumps({'key': key, 'value': value})
+        for key, value
+        in records
+    )
+    source_file.write_text(source_data)
 
-    shutil.copy(EXAMPLES_DIR / 'file_source' / 'file_source.py', tmp_with_pyconnect)
-    subprocess.run(['.test_venv/bin/python', 'file_source.py', '--config', 'env'], env=env_vars,
-                   cwd=tmp_with_pyconnect, check=True)
+    shutil.copy(EXAMPLES_DIR / 'file_source' / 'file_source.py', tmpdir)
+    subprocess.run([venv_bin / 'python', 'file_source.py', '--config', 'env'], env=env_vars,
+                   cwd=tmpdir, check=True)
 
     published_records = consume_all()
 
