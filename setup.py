ae3601d0658b2a7bb0d060f6992e534626f160ae
garrettthomas <garrettcarrollthomas@gmail.com>
2020-04-14 14:19:54 +0200

    no avro-python3 version

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
-# TODO remove avro-python3 version pin once they fixed the pycodestyle import
-required = ["confluent-kafka[avro]>=1.0", "pyaml>=3.13", "avro-python3==1.9.1"]
+required = ["confluent-kafka[avro]>=1.0", "pyaml>=3.13", "avro-python3"]
 
 
 setup(
